acdc8b062b43e12db3ff1bf980ccd7157d750d2d
lattesiber <ahmetsafasahin6106@gmail.com>
2026-04-02 18:46:51 +0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3118,7 +3118,7 @@ const Footer = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.c
   defaultMessage: "Feedback & Bugs",
   id: "tw.feedback"
 })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("a", {
-  href: "https://github.com/TurboWarp/"
+  href: "https://github.com/Scaty-Web/studio"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_intl__WEBPACK_IMPORTED_MODULE_5__["FormattedMessage"], {
   defaultMessage: "Source Code",
   id: "tw.code"
